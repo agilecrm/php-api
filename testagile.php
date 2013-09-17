26e132c9d29105098e3421f48f8c476c5287b090
karthikagile <karthik@invox.com>
2013-09-18 02:43:24 +0530

    update

--- testagile.php
+++ testagile.php
@@ -19,31 +19,31 @@ function curlWrap ($url, $json, $action)
 	CURLOPT_FOLLOWLOCATION=>true,
 	CURLOPT_MAXREDIRS=>10,
 	));
-	switch($action)
-	{
-		case "POST":
-			curl_setopt($ch,CURLOPT_URL,'https://'.domain.'.agilecrm.com/core/php/api/'.$url.'?id='.apikey);
-			curl_setopt($ch,CURLOPT_CUSTOMREQUEST,"POST");
-			curl_setopt($ch,CURLOPT_POSTFIELDS,$json);
-			break;
-		case "GET":
-			$json = json_decode($json);
-			curl_setopt($ch,CURLOPT_CUSTOMREQUEST,"GET");
-			curl_setopt($ch,CURLOPT_URL,'https://'.domain.'agilecrm.com/core/php/api/'.$url.'?id='.apikey.'&email='.$json->{'email'});
-			break;
-		case "PUT":
-			curl_setopt($ch,CURLOPT_URL,'https://'.domain.'.agilecrm.com/core/php/api/'.$url.'?id='.apikey);
-			curl_setopt($ch,CURLOPT_CUSTOMREQUEST,"PUT");
-			curl_setopt($ch,CURLOPT_POSTFIELDS,$json);
-			break;
-		case "DELETE":
-			$json = json_decode($json);
-			curl_setopt($ch,CURLOPT_URL,'https://'.domain.'.agilecrm.com/core/php/api/'.$url.'?id='.apikey.'&email='.$json->{'email'});
-			curl_setopt($ch,CURLOPT_CUSTOMREQUEST,"DELETE");
-			break;
-		default:
-			break;
-	}
+switch($action)
+{
+case "POST":
+	curl_setopt($ch,CURLOPT_URL,'https://'.domain.'.agilecrm.com/core/php/api/'.$url.'?id='.apikey);
+	curl_setopt($ch,CURLOPT_CUSTOMREQUEST,"POST");
+	curl_setopt($ch,CURLOPT_POSTFIELDS,$json);
+	break;
+case "GET":
+	$json = json_decode($json);
+	curl_setopt($ch,CURLOPT_CUSTOMREQUEST,"GET");
+	curl_setopt($ch,CURLOPT_URL,'https://'.domain.'agilecrm.com/core/php/api/'.$url.'?id='.apikey.'&email='.$json->{'email'});
+	break;
+case "PUT":
+	curl_setopt($ch,CURLOPT_URL,'https://'.domain.'.agilecrm.com/core/php/api/'.$url.'?id='.apikey);
+	curl_setopt($ch,CURLOPT_CUSTOMREQUEST,"PUT");
+	curl_setopt($ch,CURLOPT_POSTFIELDS,$json);
+	break;
+case "DELETE":
+	$json = json_decode($json);
+	curl_setopt($ch,CURLOPT_URL,'https://'.domain.'.agilecrm.com/core/php/api/'.$url.'?id='.apikey.'&email='.$json->{'email'});
+	curl_setopt($ch,CURLOPT_CUSTOMREQUEST,"DELETE");
+	break;
+default:
+	break;
+}
 	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
 	curl_setopt_array($ch, array(
 	CURLOPT_RETURNTRANSFER=>true,
@@ -110,4 +110,5 @@ curlWrap("tags", $rm_tags_json, "PUT");
 
 # To get tags assigned to a contact
 curlWrap("tags", $json, "GET");
+
 ?>
