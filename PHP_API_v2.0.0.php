51b41763437259724500be65c1a22ce04ec7c8a6
karthikagile <karthik@invox.com>
2015-10-13 12:25:57 +0530

    Modified update contact and deal
    Now above code will handle loss of data while updating contact or deal.

--- PHP_API_v2.0.0.php
+++ PHP_API_v2.0.0.php
@@ -150,80 +150,25 @@ echo "<br><hr><br>";
 
 /*================================================= update contact ================================================*/
 
-$address1 = array(
-  "address"=>"Avenida Álvares Cabral 1777",
-  "city"=>"Belo Horizonte",
+$contact = curl_wrap("contacts/search/email/ronaldo123@gmail.com", null, "GET");
+//Get contact by email and update it.
+$result = json_decode($contact, false, 512, JSON_BIGINT_AS_STRING);
+
+$result->lead_score = "150";
+$address = array(
+  "address"=>"MIG-107",
+  "city"=>"Belo Horizonto",
   "state"=>"Minas Gerais",
   "country"=>"Brazil"
 );
-
-$contact_email1 = "unique@gmail.com";
-
-
-
-$new_contact_json1 = array(
-  "id"=>"5706163895140352", 	// This contact id is related to unique@gmail.com. 
-								//And it is mandatory. Example contact id :http://snag.gy/M6S3A.jpg
-								//You can get contact id of email which is described below.
-  "lead_score"=>"24",
-  "star_value"=>"4",
-  "tags"=>array("test1","test2"),
-  "properties"=>array(
-    array(
-      "name"=>"first_name",
-      "value"=>"ArnoldUpdated",
-      "type"=>"SYSTEM"
-    ),
-    array(
-      "name"=>"last_name",
-      "value"=>"hello",
-      "type"=>"SYSTEM"
-    ),
-    array(
-      "name"=>"email",
-      "value"=>$contact_email1,
-      "type"=>"SYSTEM"
-    ),  
-    array(
-        "name"=>"title",
-        "value"=>"the phenomenon",
-        "type"=>"SYSTEM"
-    ),
-	array(
-        "name"=>"company",
-        "value"=>"ibm",
-        "type"=>"SYSTEM"
-    ),
-	array(
-        "name"=>"address",
-        "value"=>json_encode($address1),
-        "type"=>"SYSTEM"
-    ),
-    array(
-        "name"=>"phone",
-        "value"=>"+1-541-754-3030",
-        "type"=>"SYSTEM"
-    ),
-    array(
-        "name"=>"website",
-        "value"=>"http://www.google.com",
-        "type"=>"SYSTEM"
-    ),
-    array(
-        "name"=>"experience in field",
-        "value"=>"5",
-        "type"=>"CUSTOM"
-    ),
-    array(
-        "name"=>"Date Of Joining",
-        "value"=>"1438951923",  // This is epoch time in seconds.
-        "type"=>"CUSTOM"
-    )
-  )
-);
-$new_contact_json1 = json_encode($new_contact_json1);
+$address1=json_encode($address);
+setContactField("address",$address1,$result); // Set contact address with new data
+setContactField("last_name","de lama2",$result);
+setContactField("DOP","1444718961",$result); // Set DOP custom data of date type.
+setContactField("text sample test","hello text sample test",$result); // Set text sample test custom data of textfiled type
 
 
+$new_contact_json1 = json_encode($result);
 echo "<li>contact updated with following data</li><br>";
 echo "<li>" . $new_contact_json1 . "</li><br>";
 $result = curl_wrap("contacts", $new_contact_json1, "PUT");
@@ -306,30 +251,28 @@ echo "<br><hr><br>";
 
 /*===================================================== update deal================================================*/
  
- $opportunity_json = array(
-  "id"=>"5675214360805376",  // Deal ID. Example : http://snag.gy/SqMqF.jpg
-  "name"=>"test deal1 updated",
-  "description"=>"this is a test deal",
-  "expected_value"=>1000,
-  "milestone"=>"Won",      // Changed milestone type from New to Won for existing deal.
-    "custom_data"=>array(
-    array(
-      "name"=>"dataone",
-      "value"=>"xyz updated"
-    ),
-    array(
-      "name"=>"datatwo",
-      "value"=>"abc updated"
-    )
-  ),
-  "probability"=>50,
-  "close_date"=>1438949981,
-  "contact_ids"=>array("5706163895140352") // Contact ID to which deal going to added.
-);
-$opportunity_json = json_encode($opportunity_json);
+$deal = curl_wrap("opportunity/5649648836411392", null, "GET");//Get deal by deal id to update.
+$result = json_decode($deal, false, 512, JSON_BIGINT_AS_STRING); 
+
+$result->name="helo test";
+$result->expected_value="1000"; // Should not be null;
+$result->milestone="Proposal"; // Milestone name should be exactly  as in agilecrm website. http://snag.gy/xjAbc.jpg
+$result->pipeline_id="5767790501822464"; 
+
+// If you are updating milestone then pipeline_id is mandatory field. pipeline_id is the id of track,
+// Otherwise comment milestone and pipeline_id to just change other field.
+
+setDealCustom("Message","this is text custom data",$result); // Setting custom message of text type.
+setDealCustom("DOJ","11/30/2015",$result);// Setting custom message of date type.
+
+if (sizeof($result->notes) > 0) { // This code checking deal has any notes or not, don't remove this if-condition. 
+    $result->notes=$result->note_ids;
+}
+
+$opportunity_json = json_encode($result);
 echo "<li>update deal with below data</li><br>";
 echo "<li>" . $opportunity_json . "</li><br>";
-$result = curl_wrap("opportunity", $opportunity_json, "POST"); 
+$result = curl_wrap("opportunity", $opportunity_json, "PUT"); 
 echo "<li>updated deal data is ...</li><br>";
 echo "<li>" . $result . "</li>";
 echo "<br><hr><br>";
